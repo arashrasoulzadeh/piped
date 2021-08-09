5a48c7173987008352febf213222931f3b76125f
arashrasoulzadeh <arashrasoulzadeh@gmail.com>
2021-08-09 20:04:41 +0430

    better null checking

--- src/Pipes/NotNullPipe.php
+++ src/Pipes/NotNullPipe.php
@@ -9,14 +9,35 @@ class NotNullPipe extends Pipe
 {
     public function command()
     {
-        if (!isset($this->args[0])) {
-            throw new BreakPipeException();
-        }
-        if (isset($this->args[0])) {
-            if (is_null($this->args[0])) {
-                throw new BreakPipeException();
+        $indexes = $this->pickArg($this->custom_args[0]);
+        if (is_null($indexes)) {
+            $this->checkIndex(0);
+        } else {
+            if (is_array($indexes)) {
+                foreach ($indexes as $index) {
+                    $this->checkIndex($index);
+                }
+            }
+            if ($indexes === '*') {
+                foreach ($this->args as $arguman) {
+                    if (is_null($arguman)) {
+                        throw new BreakPipeException();
+                    }
+                }
             }
         }
         return $this->last_output;
+    }
+
+    public function checkIndex($index)
+    {
+        if (!isset($this->args[$index])) {
+            throw new BreakPipeException();
+        }
+        if (isset($this->args[$index])) {
+            if (is_null($this->args[$index])) {
+                throw new BreakPipeException();
+            }
+        }
     }
 }
