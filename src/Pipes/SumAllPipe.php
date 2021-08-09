9ad3f9f455fada610cbf2001c4753e971a696ba7
arashrasoulzadeh <arashrasoulzadeh@gmail.com>
2021-08-09 20:07:52 +0430

    sum all pipe

---
+++ src/Pipes/SumAllPipe.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace arashrasoulzadeh\piped\Pipes;
+
+use arashrasoulzadeh\piped\Abstracts\Pipe;
+use arashrasoulzadeh\piped\Exceptions\BreakPipeException;
+
+/**
+ * Sum all items in args array
+ */
+class SumAllPipe extends Pipe
+{
+    public function command()
+    {
+        foreach ($this->args as $arg) {
+            if (!is_numeric($arg)) {
+                throw new BreakPipeException();
+            }
+            $this->last_output += $arg;
+        }
+        return $this->last_output;
+    }
+}
