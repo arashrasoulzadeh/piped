470107c3b38f343a2d993e52a9686431df9b895c
arashrasoulzadeh <arashrasoulzadeh@gmail.com>
2021-08-11 02:54:04 +0430

    multiple pipes test support

--- tests/unit/PipeLineCest.php
+++ tests/unit/PipeLineCest.php
@@ -1,18 +1,44 @@
 <?php
 use arashrasoulzadeh\piped\Piped;
+use arashrasoulzadeh\piped\Pipes\MapPipe;
 
 class PipeLineCest
 {
 
     // tests
-    public function sampleEmptyPipeTest(UnitTester $I)
+    public function emptyPipeTest(UnitTester $I)
     {
         $pipe = Piped::build()->pipe(null);
         $I->assertNull($pipe->output());
     }
-     public function sampleEmptyPipeThroughTest(UnitTester $I)
+    public function emptyPipeThroughTest(UnitTester $I)
     {
         $pipe = Piped::build()->pipe(null)->through();
         $I->assertNull($pipe->output()[0]);
+    }
+    public function mapPipeThroughTest(UnitTester $I)
+    {
+        $pipe = Piped::build()->pipe(2)->through(
+            [MapPipe::class,function($item,$index){
+                return $item*2;
+            }]
+        );
+        $I->assertEquals($pipe->output()[0],4);
+    }
+    public function multipleMapPipeThroughTest(UnitTester $I)
+    {
+        $pipe = Piped::build()->pipe(2)->through(
+            [MapPipe::class,function($item,$index){
+                return $item*2;
+            }],
+            [MapPipe::class,function($item,$index){
+                return $item+2;
+            }],
+            [MapPipe::class,function($item,$index){
+                return $item/2;
+            }],
+
+        );
+        $I->assertEquals($pipe->output()[0],3);
     }
 }
